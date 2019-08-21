035156fe3914ab17dc33f5e7d04b07f367c601b7
Jeremy Purser <jeremypurser@gmail.com>
2019-08-21 15:22:26 -0400

    Add body-parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const parser = require('body-parser');
 
 app.use(express.static(__dirname + '/../public'));
 app.listen(port, () => {
