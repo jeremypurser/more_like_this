2f95d31bfa62dd55d03b102693bf164a1de4db5a
Jeremy Purser <jeremypurser@gmail.com>
2019-08-21 17:15:25 -0400

    Initialize schema

--- database/index.js
+++ database/index.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+let mongoURL = 'mongodb://localhost/imongodb';
+mongoose.connect(mongoURL);
+
+let movieSchema = mongoose.Schema({
+  name:
+  year:
+  ageRating:
+  genre:
+  coverImage:
+  reviewRating:
+  summary:
+  director:
+  stars:
+});
