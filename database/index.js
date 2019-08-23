5ca677549143e86f1f1a74378e8eb4b177d4a65d
Jeremy Purser <jeremypurser@gmail.com>
2019-08-23 17:13:28 -0400

    Add { useNewUrlParser: true } for Jest deprecation warning

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURL = 'mongodb://localhost/imongodb';
 
-const db = mongoose.connect(mongoURL);
+const db = mongoose.connect(mongoURL, { useNewUrlParser: true });
 
 module.exports = db;
