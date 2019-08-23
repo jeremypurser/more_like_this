8b2bbf28f8363fa74084ded4240e4858acd2834f
Jeremy Purser <jeremypurser@gmail.com>
2019-08-23 18:38:54 -0400

    Adjust mongo URL

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ let mongoURL;
 if (process.env.NODE_ENV !== 'test') {
   mongoURL = 'mongodb://localhost/imongodb';
 } else {
-  mongoURL = 'localhost:27017';
+  mongoURL = 'mongodb://localhost:27017';
 }
 
 const db = mongoose.connect(mongoURL, { useNewUrlParser: true });
