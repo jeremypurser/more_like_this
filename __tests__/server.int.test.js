2dc5dd3465a624e56b7beb2124fc8686f7769628
Jeremy Purser <jeremypurser@gmail.com>
2019-08-23 20:25:21 -0400

    Add comment to override default test environment

--- __tests__/server.int.test.js
+++ __tests__/server.int.test.js
@@ -1,14 +1,17 @@
+/**
+ * @jest-environment node
+ */
 const request = require('supertest');
 const app = require('../server');
 
-xdescribe('GET /movies', () => {
+describe('GET /movies', () => {
   test('should return an array', async () => {
     const response = await request(app).get('/movies');
     expect(response.body instanceof Array).toBe(true);
   });
 });
 
-xdescribe('GET /movies', () => {
+describe('GET /movies', () => {
   test('response array lenght === 12', async () => {
     const response = await request(app).get('/movies');
     expect(response.body.length).toBe(12);
