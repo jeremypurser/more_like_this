fc2f02a319e99994b1a4eba3606652f1a511dce8
Jeremy Purser <jeremypurser@gmail.com>
2019-08-24 18:53:40 -0400

    Set up styled components and prev/next click handlers

--- client/src/components/More.jsx
+++ client/src/components/More.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const Frame = styled.span`
+  display: inline-block;
+  width: 70px;
+  height: 100px;
+  padding: 3px;
+`;
+
+const Container = styled.div`
+  width: 240px;
+`;
 
 class More extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: []
+      movies: [],
+      mosaic: []
     };
   }
 
@@ -16,12 +34,20 @@ class More extends React.Component {
       .then(movies => {
         console.log(movies);
         this.setState({ movies });
+        this.setState({ mosaic: movies.slice(0, 6)});
       });
   }
 
   render() {
     return (
-      <div>Hello world</div>
+      <Container>
+        <p>More Like This</p>
+        <div>
+          {this.state.mosaic.map(movie => <Frame key={movie._id}><Img key={movie._id} src={movie.coverImage} /></Frame>)}
+          <a onClick={() => { this.setState({ mosaic: this.state.movies.slice(0, 6) }); }}>&larr; Prev 6 </a>
+          <a onClick={() => { this.setState({ mosaic: this.state.movies.slice(6) }); }}>Next 6 &rarr;</a>
+        </div>
+      </Container>
     );
   }
 }
