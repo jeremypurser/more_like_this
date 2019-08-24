fbff32a3b329c76772d631221f51ab17d75f642c
Jeremy Purser <jeremypurser@gmail.com>
2019-08-23 20:12:29 -0400

    Disable server test

--- __tests__/server.int.test.js
+++ __tests__/server.int.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('GET /movies', () => {
+xdescribe('GET /movies', () => {
   test('should return an array', async () => {
     const response = await request(app).get('/movies');
     expect(response.body instanceof Array).toBe(true);
   });
 });
 
-describe('GET /movies', () => {
+xdescribe('GET /movies', () => {
   test('response array lenght === 12', async () => {
     const response = await request(app).get('/movies');
     expect(response.body.length).toBe(12);
