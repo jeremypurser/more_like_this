fa7b7a785d99876220ee00b9d2d9f79c24848d3f
Jeremy Purser <jeremypurser@gmail.com>
2019-08-27 16:21:13 -0400

    Add unit tests for More component

--- __tests__/More.unit.test.js
+++ __tests__/More.unit.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import More from '../client/src/components/More';
+import { shallow, mount, render } from 'enzyme';
+import More from '../client/src/components/More.jsx';
 
-describe('First Enzyme test', () => {
+describe('<More />', () => {
   test('renders without crashing', () => {
     shallow(<More />);
   });
+
+  test('renders a div', () => {
+    const wrapper = shallow(<More />);
+    expect(wrapper.find('div').length).toBe(1);
+  });
+
+  test('renders two <a>', () => {
+    const wrapper = mount(<More />);
+    expect(wrapper.find('a').length).toBe(2);
+  });
+
+  test('renders one <p>', () => {
+    const wrapper = mount(<More />);
+    expect(wrapper.find('p').length).toBe(1);
+  });
 });
+
