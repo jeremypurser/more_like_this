a8f00e2720bd62e0cc235da43a64dc97ab670797
Jeremy Purser <jeremypurser@gmail.com>
2019-08-29 19:36:59 -0400

    Add state for highlighted along with methods to change

--- client/src/components/More.jsx
+++ client/src/components/More.jsx
@@ -4,6 +4,7 @@ import {
   Img, Frame, StyleDiv2, View, Container, Prev, Next, Arrow
 } from './StyledComponents.jsx';
 
+
 class More extends React.Component {
   constructor() {
     super();
@@ -22,29 +23,63 @@ class More extends React.Component {
         return response.ok ? response.json() : response;
       })
       .then(movies => {
-        this.setState({ movies });
-        this.setState({ prev: movies.slice(0, 6) });
-        this.setState({ next: movies.slice(6) });
-        this.setState({ highlighted: movies[0] });
+        this.setState({
+          movies,
+          prev: movies.slice(0, 6),
+          next: movies.slice(6),
+          highlighted: 0
+        });
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-  becomeHighlight(e) {
-    this.setState({
-      highlighted: this.state.movies[e.target.id]
-    });
-  }
-
   componentDidMount() {
     this.fetchData();
   }
 
+  becomeHighlight(e) {
+    this.setState({
+      highlighted: Number(e.target.id)
+    });
+  }
+
+  handlePrev() {
+    this.setState({
+      mosaic: 'prev',
+      highlighted: 0
+    });
+  }
+
+  handleNext() {
+    this.setState({
+      mosaic: 'next',
+      highlighted: 6
+    });
+  }
+
+  incrementHighlight() {
+    let current = this.state.highlighted;
+    if (current === 11) {
+      current = -1;
+    }
+    let highlighted = current + 1;
+    this.setState({ highlighted });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.highlighted === 5 && this.state.highlighted === 6) {
+      this.setState({mosaic: 'next'});
+    } else if (prevState.highlighted === 11 && this.state.highlighted === 0) {
+      this.setState({mosaic: 'prev'});
+    }
+  }
+
   render() {
     let HighlightDiv = this.state.highlighted === null ? <div></div> :
-      <Hightlight movie={this.state.highlighted} />;
+      <Hightlight movie={this.state.movies[this.state.highlighted]}
+        increment={this.incrementHighlight.bind(this)} />;
 
     return (
       <div>
@@ -55,6 +90,7 @@ class More extends React.Component {
               <Frame key={idx}>
                 <Img id={idx} key={idx}
                   onPointerEnter={(e) => { this.becomeHighlight(e); }}
+                  highlighted={this.state.highlighted}
                   src={movie.coverImage} />
               </Frame>)
             )}
@@ -64,6 +100,7 @@ class More extends React.Component {
               <Frame key={idx}>
                 <Img id={idx + 6} key={idx + 6}
                   onPointerEnter={(e) => { this.becomeHighlight(e); }}
+                  highlighted={this.state.highlighted}
                   src={movie.coverImage} />
               </Frame>)
             )}
@@ -71,10 +108,10 @@ class More extends React.Component {
         </Container>
         <Arrow>
           <Prev mosaic={this.state.mosaic}
-            onClick={() => { this.setState({ mosaic: 'prev' }); }}>&larr; Prev 6</Prev>
+            onClick={this.handlePrev.bind(this)}>&larr; Prev 6</Prev>
           <span> | </span>
           <Next mosaic={this.state.mosaic}
-            onClick={() => { this.setState({ mosaic: 'next' }); }}>Next 6 &rarr;</Next>
+            onClick={this.handleNext.bind(this)}>Next 6 &rarr;</Next>
         </Arrow>
         {HighlightDiv}
         <StyleDiv2 />
