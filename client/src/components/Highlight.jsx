eb46c5e094749d01eb5d6a530e1a73dfad59f2ea
Jeremy Purser <jeremypurser@gmail.com>
2019-08-30 17:48:51 -0400

    Add styles for movie details

--- client/src/components/Highlight.jsx
+++ client/src/components/Highlight.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import { HighlightDiv, HighlightImg,
-  HighlightFrame, Info } from './StyledComponents.jsx';
+import { HighlightDiv, HighlightImg, Title, Year,
+  HighlightFrame, Info, MPAA, Genre, Summary,
+  H3, Btn1, Btn2 } from './StyledComponents.jsx';
+import styled, { css, ThemeProvider } from 'styled-components';
 
-const Highlight = ({movie, increment}) => (
-  <HighlightDiv>
-    <HighlightFrame>
-      <HighlightImg src={movie.coverImage} />
-      <button>Add to Watchlist</button>
-      <button onClick={increment}>Next &rarr;</button>
-    </HighlightFrame>
-    <Info>
-      <h3>{movie.title} <span>({movie.year})</span></h3>
-      <p><span>{movie.mpaaRating}</span>{movie.genre}</p>
-      <p>{movie.summary}</p>
-      <p>Director: {movie.director}</p>
-      <p>Stars: {movie.stars}</p>
-    </Info>
-  </HighlightDiv>
-);
+
+const Highlight = ({movie, increment}) => {
+  const ratingTheme = (rating) => {
+    if (rating === 'G') {
+      return {
+        width: '17px',
+        position: '-72px -3px'
+      };
+    } else if (rating === 'PG') {
+      return {
+        width: '27px',
+        position: '-91px -3px'
+      };
+    } else if (rating === 'PG-13') {
+      return {
+        width: '44px',
+        position: '-119px -3px'
+      };
+    } else if (rating === 'R') {
+      return {
+        width: '18px',
+        position: '-165px -3px'
+      };
+    }
+  };
+
+  return (
+    <HighlightDiv>
+      <HighlightFrame>
+        <HighlightImg src={movie.coverImage} />
+        <Btn1>Add to Watchlist</Btn1>
+        <Btn2 onClick={increment}>Next &rarr;</Btn2>
+      </HighlightFrame>
+      <Info>
+        <H3><Title>{movie.title}</Title> <Year>({movie.year})</Year></H3>
+        <Genre>
+          <ThemeProvider theme={() => ratingTheme(movie.mpaaRating)}>
+            <MPAA />
+          </ThemeProvider> {movie.genre.join(' | ')}
+        </Genre>
+        <p>Rating: {movie.reviewRating}</p>
+        <Summary>{movie.summary}</Summary>
+        <Summary><strong>Director:</strong> {movie.director}</Summary>
+        <Summary><strong>Stars:</strong> {movie.stars.join(', ')}</Summary>
+      </Info>
+    </HighlightDiv>
+  );
+};
 
 export default Highlight;
