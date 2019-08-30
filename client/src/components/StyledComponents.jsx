2a5714ef0c3b6b5b48e639cab490040e8dea431a
Jeremy Purser <jeremypurser@gmail.com>
2019-08-30 17:47:50 -0400

    Add styles for movie details

--- client/src/components/StyledComponents.jsx
+++ client/src/components/StyledComponents.jsx
@@ -26,7 +26,6 @@ const HighlightDiv = styled.div`
   position: relative;
   height: 300px;
   width: 600px;
-  border: solid 1px red;
   background: white;
   top: -315px;
   left: 295px;
@@ -34,10 +33,10 @@ const HighlightDiv = styled.div`
   z-index: 1;
 `;
 
-const StyleDiv2 = styled(HighlightDiv)`
+const Spacer = styled(HighlightDiv)`
   width: 4px;
-  left: -8px;
-  top: -615px;
+  left: -10px;
+  top: -625px;
 `;
 
 const Info = styled.div`
@@ -58,7 +57,7 @@ const View = styled.div`
   width: 296px;
 `;
 
-const Container = styled.div`
+const ViewContainer = styled.div`
   width: 600px;
   height: 275px;
   overflow: hidden;
@@ -67,20 +66,103 @@ const Container = styled.div`
   margin-bottom: 10px;
 `;
 
+const Container = styled.div`
+  font-family: Verdana, sans-serif;
+`;
+
 const Prev = styled.a`
   cursor: pointer;
-  color: ${props => props.mosaic === 'next' ? 'black' : '#878787' };
+  font-size: 13px;
+  color: ${props => props.mosaic === 'next' ? '#136cb2' : '#ccc' };
 `;
 
 const Next = styled(Prev)`
-  color: ${props => props.mosaic === 'next' ? '#878787' : 'black' };
+  color: ${props => props.mosaic === 'next' ? '#ccc' : '#136cb2' };
 `;
 
 const Arrow = styled.div`
-  margin-left: 70px;
+  margin-left: 80px;
+`;
+
+const MPAA = styled.span`
+  display: inline-block;
+  position: relative;
+  top: 4px;
+  background-image: url('https://hrr40-fec2-jeremypurser.s3.us-east-2.amazonaws.com/titlePageSprite.png');
+  background-position: ${props => props.theme.position};
+  height: 16px;
+  width: ${props => props.theme.width};
+  display: inline-block;
+`;
+
+const Genre = styled.div`
+  font-size: 11px;
+  color: #aaa;
+  margin-top: -8px;
+`;
+
+const H2 = styled.h2`
+  font: 24px Helvetica, Arial, sans-serif;
+  font-weight: normal;
+  color: #424242;
+`;
+
+const H3 = styled.h3`
+  line-height: 1.1;
+`;
+
+const Title = styled.a`
+  color: #136cb2;
+  font-size: 13px;
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  &:visited {
+    color: #70579d;
+  }
+`;
+
+const Year = styled.span`
+  font-size: 13px;
+  color: #999;
+  font-weight: normal;
+`;
+
+const Summary = styled.p`
+  color: #333;
+  line-height: 1.4;
+  font-size: 11px;
+`;
+
+const Btn1 = styled.button`
+  display: block;
+  font-family: Verdana, Arial, sans-serif;
+  text-align: center;
+  font-size: 13px;
+  width: 98%;
+  margin: 2px auto 0;
+  background-image: linear-gradient(#fefcea, #f3ce00);
+  &:focus {
+    outline: none;
+    text-decoration: none;
+  }
+  &:hover {
+    outline: none;
+    text-decoration: none;
+  }
+`;
+
+const Btn2 = styled(Btn1)`
+  background-image: linear-gradient(#faf8f1, #ece2c6);
+  &:hover {
+    background-color: #ece2c6;
+    border: 1px solid #e6b800;
+  }
 `;
 
 export {
-  Img, StyleDiv2, View, Container, Prev, Next, Arrow,
-  HighlightImg, Frame, HighlightDiv, HighlightFrame, Info
+  Img, Container, Spacer, View, ViewContainer, Prev, Next, Arrow,
+  HighlightImg, Frame, HighlightDiv, HighlightFrame, Info, MPAA,
+  Genre, H2, Title, Year, Summary, Btn1, Btn2, H3
 };
