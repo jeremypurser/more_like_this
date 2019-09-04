525b32fab0cb0f2869d2cd49ec2e4b1ed3637c9b
Jeremy Purser <jeremypurser@gmail.com>
2019-09-03 22:41:57 -0400

    Adjust code style

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
+const cors = require('cors');
 const Movie = require('../database/Movie.js');
 const getRandomDocs = require('../utils');
 const port = 3001;
 
 app.use(express.static(__dirname + '/../public'));
+app.use(cors({
+  origin: function(origin, next) {
+    var allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:3001'
+    ];
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const message = 'The CORS policy for this site does not \
+        allow access from the specified Origin.';
+      return next(new Error(message), false);
+    }
+    return next(null, true);
+  }
+}));
 
 app.get('/movies', (req, res) => {
   Movie.find({}, (err, docs) => {
