7040cb974e27f37a3c361a1fdca6f6e3329c8f75
Jeremy Purser <jeremypurser@gmail.com>
2019-09-07 18:38:46 -0400

    Remove ErrorBoundary

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ErrorBoundary from './components/ErrorBoundary.jsx';
 import More from './components/More.jsx';
 
-ReactDOM.render(<ErrorBoundary><More /></ErrorBoundary>,
+ReactDOM.render(<More />,
   document.getElementById('more'));
